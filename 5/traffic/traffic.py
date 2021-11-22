c9f22305d7c5be7889920212af5a922a28ef4786
luisboris <boris.619@gmail.com>
2021-11-22 12:13:58 +0000

    l

--- 5/traffic/traffic.py
+++ 5/traffic/traffic.py
@@ -3,13 +3,15 @@ import numpy as np
 import os
 import sys
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
 from sklearn.model_selection import train_test_split
 
 EPOCHS = 10
 IMG_WIDTH = 30
 IMG_HEIGHT = 30
-NUM_CATEGORIES = 43
+NUM_CATEGORIES = 3
 TEST_SIZE = 0.4
 
 
@@ -23,7 +25,7 @@ def main():
     images, labels = load_data(sys.argv[1])
 
     # Split data into training and testing sets
-    labels = tf.keras.utils.to_categorical(labels)
+    labels = keras.utils.to_categorical(labels)
     x_train, x_test, y_train, y_test = train_test_split(
         np.array(images), np.array(labels), test_size=TEST_SIZE
     )
@@ -58,16 +60,52 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
-    raise NotImplementedError
+    images, labels = ([], [])
 
-
+    # repeat for every folder (#folder = #label)
+    for i in range(NUM_CATEGORIES):
+        # read image, resize it and store it
+        for file in os.listdir(os.path.join(data_dir, str(i))):
+            img = cv2.imread(os.path.join(data_dir, str(i), file))
+            img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+            images.append(img)
+            labels.append(i)
+            #cv2.imshow("display window", img)
+            #cv2.waitKey(0)
+    
+    return(images, labels)
+    
 def get_model():
     """
     Returns a compiled convolutional neural network model. Assume that the
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    model = keras.models.Sequential()
+    # hidden layers
+    model.add(keras.layers.Dense(5, input_shape=(IMG_WIDTH, IMG_HEIGHT, 3), activation='relu'))
+    # output layer
+    model.add(keras.layers.Dense(1, activation='sigmoid'))
+
+    model
+    print(model)
+
+    # convolution
+
+    # pooling
+
+    # dropout
+
+    # training
+    model.compile(
+        optimizer='adam',
+        loss="sparse_categorical_crossentropy",
+        metrics=['accuracy']
+    )
+
+    print(model.summary())
+    
+    return model
 
 
 if __name__ == "__main__":
