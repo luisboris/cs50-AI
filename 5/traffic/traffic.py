a8a2874e60e8d0563db399802528bd7f2f8b253e
luisboris <boris.619@gmail.com>
2021-11-23 21:23:33 +0000

    k

--- 5/traffic/traffic.py
+++ 5/traffic/traffic.py
@@ -7,11 +7,12 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 from sklearn.model_selection import train_test_split
+from tensorflow.python.framework.ops import NullContextmanager
 
 EPOCHS = 10
 IMG_WIDTH = 30
 IMG_HEIGHT = 30
-NUM_CATEGORIES = 3
+NUM_CATEGORIES = 43
 TEST_SIZE = 0.4
 
 
@@ -60,7 +61,8 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
-    images, labels = ([], [])
+    images = list()
+    labels = list()
 
     # repeat for every folder (#folder = #label)
     for i in range(NUM_CATEGORIES):
@@ -73,7 +75,7 @@ def load_data(data_dir):
             #cv2.imshow("display window", img)
             #cv2.waitKey(0)
     
-    return(images, labels)
+    return images, labels
     
 def get_model():
     """
@@ -81,16 +83,15 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    model = keras.models.Sequential()
-    # hidden layers
-    model.add(keras.layers.Dense(5, input_shape=(IMG_WIDTH, IMG_HEIGHT, 3), activation='relu'))
-    # output layer
-    model.add(keras.layers.Dense(1, activation='sigmoid'))
 
-    model
-    print(model)
-    fero
-    
+    model = keras.models.Sequential([
+        layers.Flatten(),
+        # hidden layers
+        layers.Dense(50, activation=tf.nn.relu),
+        # output layer
+        layers.Dense(NUM_CATEGORIES, activation=tf.nn.softmax)
+    ])
+
     # convolution
 
     # pooling
@@ -99,13 +100,11 @@ def get_model():
 
     # training
     model.compile(
-        optimizer='adam',
-        loss="sparse_categorical_crossentropy",
-        metrics=['accuracy']
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
     )
 
-    print(model.summary())
-    
     return model
 
 
